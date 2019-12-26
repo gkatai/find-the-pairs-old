6e660e1cf8f06729aa8c974c63e0292c0a9b8e16
Gergely Kátai <katai.gergely@gmail.com>
2019-12-26 22:57:01 +0100

    feat(board): implemented according to tests

--- src/game-logic/board.js
+++ src/game-logic/board.js
@@ -1,3 +1,5 @@
+import produce from 'immer';
+
 const board = {
   ifFlipIsValid,
   flip,
@@ -7,10 +9,61 @@ const board = {
 
 export default board;
 
-function ifFlipIsValid() {}
+function ifFlipIsValid(index, blocks) {
+  if (index > blocks.length - 1 || index < 0 || blocks[index] === 0) {
+    return false;
+  }
 
-function flip() {}
+  return true;
+}
 
-function evaluate() {}
+function flip(index, flippedElements) {
+  let result;
 
-function flipAndEvaluate() {}
+  if (flippedElements.length === 2) {
+    result = [];
+  } else {
+    result = [...flippedElements];
+  }
+
+  result.push(index);
+
+  return result;
+}
+
+function evaluate(flippedElements, blocks) {
+  if (flippedElements.length === 2 && blocks[flippedElements[0]] === blocks[flippedElements[1]]) {
+    const updatedBlocks = [...blocks];
+    updatedBlocks[flippedElements[0]] = 0;
+    updatedBlocks[flippedElements[1]] = 0;
+    return { updatedBlocks, isMatch: true };
+  }
+
+  return { isMatch: false };
+}
+
+function flipAndEvaluate(state) {
+  if (board.ifFlipIsValid(state.selectedIndex, state.board.blocks)) {
+    const flippedElements = board.flip(state.selectedIndex, state.flippedElements);
+    const evaluationResult = board.evaluate(flippedElements, state.board.blocks);
+
+    if (evaluationResult.isMatch) {
+      return produce(state, draftState => {
+        draftState.flippedElements = flippedElements;
+        draftState.selectedIndex = undefined;
+        draftState.board.isLocked = true;
+        draftState.board.blocks = evaluationResult.updatedBlocks;
+      });
+    } else {
+      return produce(state, draftState => {
+        draftState.flippedElements = board.flip(state.selectedIndex, state.flippedElements);
+        draftState.selectedIndex = undefined;
+      });
+    }
+  }
+
+  return produce(state, draftState => {
+    draftState.invalidMessage = true;
+    draftState.selectedIndex = undefined;
+  });
+}
--- src/game-logic/board.test.js
+++ src/game-logic/board.test.js
@@ -1,29 +1,155 @@
-// eslint-disable-next-line no-unused-vars
 import board from './board';
 
 describe('Board', () => {
   describe('ifFlipIsValid', () => {
-    test.todo('should return true if selected element is not yet flipped');
-    test.todo('should return false if selected element is already flipped');
-    test.todo('should return false if selected element is bigger then array size');
-    test.todo('should return false if selected element is smaller than array size');
+    it('should return true if selected element is not yet flipped', () => {
+      const result = board.ifFlipIsValid(0, [1, 1, 2, 2]);
+
+      expect(result).toBe(true);
+    });
+
+    it('should return false if selected element is already flipped', () => {
+      const result = board.ifFlipIsValid(0, [0, 0, 2, 2]);
+
+      expect(result).toBe(false);
+    });
+
+    it('should return false if selected element is bigger then array size', () => {
+      const result = board.ifFlipIsValid(4, [1, 1, 2, 2]);
+
+      expect(result).toBe(false);
+    });
+
+    it('should return false if selected element is smaller than array size', () => {
+      const result = board.ifFlipIsValid(-1, [1, 1, 2, 2]);
+
+      expect(result).toBe(false);
+    });
   });
+
   describe('flip', () => {
-    test.todo('should return with an array containing the added element if flip array was empty');
-    test.todo('should return with an array containing the first and the added element if array had an element');
-    test.todo('should return with an array containing the the added element if array had second element');
+    it("should return with an array containing the added element's index if flip array was empty", () => {
+      const result = board.flip(0, []);
+
+      expect(result).toEqual([0]);
+    });
+
+    it("should return with an array containing the first and the added element's index if array had an element", () => {
+      const result = board.flip(1, [0]);
+
+      expect(result).toEqual([0, 1]);
+    });
+
+    it("should return with an array containing the the added element's index if array had second element", () => {
+      const result = board.flip(2, [0, 1]);
+
+      expect(result).toEqual([2]);
+    });
   });
+
   describe('evaluate', () => {
-    describe('when flip array elements are matching', () => {
-      test.todo('should return with the updated blocks array and true match property');
+    it('when flip array is empty', () => {
+      const expectedResult = { isMatch: false };
+
+      const result = board.evaluate([], [1, 1, 2, 2]);
+
+      expect(result).toEqual(expectedResult);
     });
-    describe('when flip array elements are not matching', () => {
-      test.todo('should return with false match property');
+
+    it('when flip array count is 1', () => {
+      const expectedResult = { isMatch: false };
+
+      const result = board.evaluate([0], [1, 1, 2, 2]);
+
+      expect(result).toEqual(expectedResult);
+    });
+
+    it('when flip array elements are matching', () => {
+      const expectedResult = { updatedBlocks: [0, 0, 2, 2], isMatch: true };
+
+      const result = board.evaluate([0, 1], [1, 1, 2, 2]);
+
+      expect(result).toEqual(expectedResult);
+    });
+
+    it('when flip array elements are not matching', () => {
+      const expectedResult = { isMatch: false };
+
+      const result = board.evaluate([0, 2], [1, 1, 2, 2]);
+
+      expect(result).toEqual(expectedResult);
     });
   });
+
   describe('flip and evaluate', () => {
-    test.todo('should check if ifFlipIsValid function has been called');
-    test.todo('should check if flip function has been called');
-    test.todo('should check if evaluate function has been called');
+    it('when invalid block is flipped', () => {
+      const state = {
+        board: {
+          blocks: [1, 1, 2, 2],
+          isLocked: false
+        },
+        selectedIndex: -1,
+        flippedElements: []
+      };
+      const expectedResult = {
+        board: {
+          blocks: [1, 1, 2, 2],
+          isLocked: false
+        },
+        selectedIndex: undefined,
+        invalidMessage: true,
+        flippedElements: []
+      };
+
+      const result = board.flipAndEvaluate(state);
+
+      expect(result).toEqual(expectedResult);
+    });
+
+    it('when valid block is flipped and is the first flipped one', () => {
+      const state = {
+        board: {
+          blocks: [1, 1, 2, 2],
+          isLocked: false
+        },
+        selectedIndex: 0,
+        flippedElements: []
+      };
+      const expectedResult = {
+        board: {
+          blocks: [1, 1, 2, 2],
+          isLocked: false
+        },
+        selectedIndex: undefined,
+        flippedElements: [0]
+      };
+
+      const result = board.flipAndEvaluate(state);
+
+      expect(result).toEqual(expectedResult);
+    });
+
+    it('when valid block is flipped and is the second flipped one and is match', () => {
+      const state = {
+        board: {
+          blocks: [1, 1, 2, 2],
+          isLocked: false
+        },
+        selectedIndex: 1,
+        flippedElements: [0]
+      };
+      const expectedResult = {
+        board: {
+          blocks: [0, 0, 2, 2],
+          isLocked: true
+        },
+        selectedIndex: undefined,
+        flippedElements: [0, 1]
+      };
+
+      const result = board.flipAndEvaluate(state);
+
+      expect(result).toEqual(expectedResult);
+    });
   });
 });
