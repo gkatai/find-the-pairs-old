ef99a0b5c49c390a803021ce8c86e4155febd22d
Gergely Kátai <katai.gergely@gmail.com>
2019-12-28 16:56:50 +0100

    refactor(index.js): code cleanup

--- src/calculator.test.js
+++
@@ -1,7 +0,0 @@
-import add from './calculator';
-
-describe('Calcualtor', () => {
-  it('should add two numbers', () => {
-    expect(add(1, 2)).toBe(3);
-  });
-});
--- src/index.js
+++ src/index.js
@@ -1,23 +1,29 @@
+// eslint-disable-next-line no-unused-vars
+import { h, render } from 'petit-dom';
 import { createStore, combineReducers } from 'redux';
 import { boardReducer } from './game-logic/board/board-reducer';
 import * as boardActions from './game-logic/board/board-actions';
 
 const store = createStore(combineReducers({ boardReducer }));
 
-console.log(store.getState());
-
-store.dispatch(boardActions.increment(2));
-
-console.log(store.getState());
-
-// eslint-disable-next-line no-unused-vars
-import { h, render } from 'petit-dom';
-
-//  assuming your HTML contains a node with "root" id
 const parentNode = document.getElementById('app');
 
-// mount
-render(<h1>Hello world!</h1>, parentNode);
+view(store.getState());
 
-// patch
-render(<h1>Hello again</h1>, parentNode);
+store.subscribe(() => {
+  view(store.getState());
+});
+
+function view(state) {
+  render(
+    <div>
+      <p onclick={handleIncrementClick}>Increment</p>
+      <p>Count: {state.boardReducer.count}</p>
+    </div>,
+    parentNode
+  );
+}
+
+function handleIncrementClick() {
+  store.dispatch(boardActions.increment(1));
+}
